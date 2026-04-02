59c4bf7e21ade643f2e1419c71c745ce3b8c0fc4
Maubry94 <matthieu.aubry94370@gmail.com>
2026-04-02 14:57:10 +0200

    feat: add style to step form

--- scripts/vue/designSystem/index.ts
+++ scripts/vue/designSystem/index.ts
@@ -3,4 +3,7 @@ import "./styles.scss";
 export { default as RepeatAddButton } from "./RepeatAddButton.vue";
 export { default as RepeatRemoveButton } from "./RepeatRemoveButton.vue";
 export { default as RepeatResetButton } from "./RepeatResetButton.vue";
+export { default as StepNextButton } from "./StepNextButton.vue";
+export { default as StepPreviousButton } from "./StepPreviousButton.vue";
+export { default as StepResetButton } from "./StepResetButton.vue";
 export { default as UnionSelectKind } from "./UnionSelectKind.vue";
